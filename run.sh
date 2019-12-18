4cce0b7367d524bc1e41c3855f6826744bc9f41b
AcrylicShrimp <led789zxpp@naver.com>
2019-12-18 11:28:16 +0900

    Changed from venv to virtualenv.

--- run.sh
+++ run.sh
@@ -12,7 +12,8 @@ echo '##############################################################'
 
 echo 'Setting up virtual environment...'
 
-python3 -m venv env
+python3 -m pip install virtualenv
+python3 -m virtualenv env
 source env/bin/activate
 
 python3 -m pip install numpy tensorflow-gpu
